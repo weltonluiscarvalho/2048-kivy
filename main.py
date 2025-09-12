03899aea2346dcc1e19d44ab2b36ad8e5694e2e4
Welton Carvalho <welton.carvalho.dev@gmail.com>
2025-09-12 09:39:53 -0300

    style: put kv language code in a separate file

--- main.py
+++ main.py
@@ -8,21 +8,6 @@ from kivy.core.window import Window
 Window.size = (400, 700)
 
 
-KV = '''
-Board:
-    Widget:
-        pos_hint: {"center_x": .5, "center_y": .5}
-        size_hint: None, None
-        size: 300, 300
-        canvas:
-            Color:
-                rgba: .70, .70, .70, 1
-            Rectangle:
-                pos: self.pos
-                size: self.size
-'''
-
-
 class Board(FloatLayout):
     pass
 
@@ -30,8 +15,7 @@ class Board(FloatLayout):
 class GameApp(App):
 
     def build(self):
-        kv = Builder.load_string(KV)
-        return kv
+        return Board()
 
 
 if __name__ == "__main__":
