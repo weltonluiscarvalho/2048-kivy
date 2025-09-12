3b7be94b6c61180844634764a5fd61306affaba3
Welton Carvalho <welton.carvalho.dev@gmail.com>
2025-09-12 09:13:42 -0300

    feat: adjust window and board size

--- main.py
+++ main.py
@@ -2,17 +2,24 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.lang.builder import Builder
 from kivy.uix.floatlayout import FloatLayout
+from kivy.core.window import Window
+
+
+Window.size = (400, 700)
 
 
 KV = '''
 Board:
     Widget:
+        pos_hint: {"center_x": .5, "center_y": .5}
+        size_hint: None, None
+        size: 300, 300
         canvas:
             Color:
                 rgba: .70, .70, .70, 1
             Rectangle:
-                pos: root.width/2 - 150, root.height/2 - 150
-                size: 300, 300
+                pos: self.pos
+                size: self.size
 '''
 
 
