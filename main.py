1c5e5e3ba40cc2e33ef917a60572a9edd1b1f121
Welton Carvalho <welton.carvalho.dev@gmail.com>
2025-09-13 13:27:19 -0300

    feat: move pieces according to avaiable positions

--- main.py
+++ main.py
@@ -6,15 +6,19 @@ from kivy.lang.builder import Builder
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.label import Label
 from kivy.core.window import Window
-from kivy.properties import ListProperty
-from random import randint
+from kivy.properties import ListProperty, ColorProperty
+from random import randint, random
 
 Window.size = (400, 700)
 
 
 class Piece(Label):
     position = ListProperty()
+    color_bg = ColorProperty()
 
+    def __init__(self, **kwargs):
+        self.color_bg = (random(), random(), random())
+        super().__init__(**kwargs)
 
 class Board(FloatLayout):
 
@@ -47,45 +51,142 @@ class Board(FloatLayout):
         if key == 275:
             self.go_right()
 
+
+    def set_piece_position(self, piece, row, column):
+        self.positions[piece.position[0]][piece.position[1]] = 0
+
+        ## move logic
+        pos_x = column * 75 + self.x
+        pos_y = row * 75 + self.y
+        piece.position = (row, column)
+        piece.pos = (pos_x, pos_y)
+
+        self.positions[piece.position[0]][piece.position[1]] = 1
+
     def go_up(self):
-        for piece in self.children:
-            print(f'position before = {piece.position}')
-            self.positions[piece.position[0]][piece.position[1]] = 0
-            piece.top = piece.parent.top
-            piece.position = (piece.position[0], 3)
-            print(f'position after = {piece.position}')
+        # for piece in self.children:
+        #     self.positions[piece.position[0]][piece.position[1]] = 0
+        #
+        #     piece.top = piece.parent.top
+        #
+        #     piece.position = (piece.position[0], 3)
+        #     self.positions[piece.position[0]][piece.position[1]] = 1
+        # self.insert_piece()
+
+        for row in range(2, -1, -1):
+            for column in range(4):
+                # verify if a piece exists at this position
+                if self.positions[row][column]:
+                    # verify possible new position for this piece
+                    next_row = row
+                    for upper_row in range(row + 1, 4):
+                        # verify if don't exist a piece at the next upper position
+                        if not self.positions[upper_row][column]:
+                            next_row = upper_row
+                    # find the piece, I know that's horrible, i gonna adjust this logic soon
+                    for piece in self.children:
+                        if piece.position == [row, column]:
+                            self.set_piece_position(piece, row=next_row, column=column)
         self.insert_piece()
 
     def go_down(self):
-        for piece in self.children:
-            print(piece.position)
-            piece.y = piece.parent.y
+        # for piece in self.children:
+        #     self.positions[piece.position[0]][piece.position[1]] = 0
+        #     piece.y = piece.parent.y
+        #     piece.position = (piece.position[0], 0)
+        #     self.positions[piece.position[0]][piece.position[1]] = 1
+        # self.insert_piece()
+
+        for row in range(1, 4):
+            for column in range(4):
+                # verify if a piece exists at this position
+                if self.positions[row][column]:
+                    # verify possible new position for this piece
+                    next_row = row
+                    for lower_row in range(row - 1, -1, -1):
+                        # verify if don't exist a piece at the next lower position
+                        if not self.positions[lower_row][column]:
+                            next_row = lower_row
+                    for piece in self.children:
+                        if piece.position == [row, column]:
+                            self.set_piece_position(piece, row=next_row, column=column)
         self.insert_piece()
 
     def go_left(self):
-        for piece in self.children:
-            print(piece.position)
-            piece.x = piece.parent.x
+        # for piece in self.children:
+        #     self.positions[piece.position[0]][piece.position[1]] = 0
+        #     piece.x = piece.parent.x
+        #     piece.position = (0, piece.position[1])
+        #     self.positions[piece.position[0]][piece.position[1]] = 1
+        # self.insert_piece()
+
+        for column in range(1, 4):
+            for row in range(4):
+                # verify if a piece exists at this position
+                if self.positions[row][column]:
+                    new_position = (row, column)
+                    # verify possible new position for this piece
+                    next_column = column
+                    for left_column in range(column - 1, -1, -1):
+                        # verify if don't exist a piece at the next leftmost column position
+                        if not self.positions[row][left_column]:
+                            next_column = left_column
+                    for piece in self.children:
+                        if piece.position == [row, column]:
+                            self.set_piece_position(piece, row=row, column=next_column)
         self.insert_piece()
 
     def go_right(self):
-        for piece in self.children:
-            print(piece.position)
-            piece.right = piece.parent.right
+        # for piece in self.children:
+        #     self.positions[piece.position[0]][piece.position[1]] = 0
+        #     piece.right = piece.parent.right
+        #     piece.position = (3, piece.position[1])
+        #     self.positions[piece.position[0]][piece.position[1]] = 1
+        # self.insert_piece()
+
+        for column in range(2, -1, -1):
+            for row in range(4):
+                # verify if a piece exists at this position
+                if self.positions[row][column]:
+                    new_position = (row, column)
+                    # verify possible new position for this piece
+                    next_column = column
+                    for right_column in range(column + 1, 4):
+                        # verify if don't exist a piece at the next rightmost column position
+                        if not self.positions[row][right_column]:
+                            next_column = right_column
+                    for piece in self.children:
+                        if piece.position == [row, column]:
+                            self.set_piece_position(piece, row=row, column=next_column)
         self.insert_piece()
 
     def insert_piece(self):
-        x = randint(0, 3)
-        y = randint(0, 3)
-        pos_x = x * 75 + self.x
-        pos_y = y * 75 + self.y
+        possible_positions = []
+        for row in self.positions:
+            for column in row:
+                if not column:
+                    possible_positions.append([row][column])
+
+        if not possible_positions:
+            print("there is not possible_positions, you lose")
+            return
+
+        while True:
+            row = randint(0, 3)
+            column = randint(0, 3)
+            if not self.positions[row][column]:
+                break
+
+        pos_x = column * 75 + self.x
+        pos_y = row * 75 + self.y
         new_piece = Piece()
-        new_piece.position = (x, y)
+        new_piece.position = (row, column)
         new_piece.pos = (pos_x, pos_y)
         new_piece.size_hint = None, None
         new_piece.size = 75, 75
         self.add_widget(new_piece)
-        self.positions[x][y] = 1
+        self.positions[row][column] = 1
+        print(self.positions)
 
 class GameApp(App):
     pass
