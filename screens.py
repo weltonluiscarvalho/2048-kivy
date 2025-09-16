0e6c6a4ce03d6fe126761e3ebab09ef591def6e1
Welton Carvalho <welton.carvalho.dev@gmail.com>
2025-09-16 14:06:34 -0300

    feat: organize screens and create a score for the game

---
+++ screens.py
@@ -0,0 +1,14 @@
+from kivymd.uix.screenmanager import ScreenManager
+from kivymd.uix.screen import Screen
+
+
+class GameScreenManager(ScreenManager):
+    pass
+
+
+class BoardScreen(Screen):
+    pass
+
+
+class MainScreen(Screen):
+    pass
