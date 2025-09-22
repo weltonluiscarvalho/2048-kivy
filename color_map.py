6d1399d861487ab967f2601a27a84ccaf9168d8a
Welton Carvalho <welton.carvalho.dev@gmail.com>
2025-09-21 21:42:50 -0300

    feat: save moves when quit the window

---
+++ color_map.py
@@ -0,0 +1,50 @@
+color_map = {
+    "2": {
+        "font": "#6e512d",
+        "bg": "#e3e0d8", 
+    },
+    "4": {
+        "font": "#401d01",
+        "bg": "#e6d7b1", 
+    },
+    "8": {
+        "font": "white",
+        "bg": "#f5ba7a", 
+    },
+    "16": {
+        "font": "white",
+        "bg": "#f5a045", 
+    },
+    "32": {
+        "font": "white",
+        "bg": "#f58b7a", 
+    },
+    "64": {
+        "font": "white",
+        "bg": "#f55d45", 
+    },
+    "128": {
+        "font": "white",
+        "bg": "#f2e279", 
+    },
+    "256": {
+        "font": "white",
+        "bg": "#f0db56", 
+    },
+    "512": {
+        "font": "white",
+        "bg": "#f2d93d", 
+    },
+    "1024": {
+        "font": "white",
+        "bg": "#fadc1e", 
+    },
+    "2048": {
+        "font": "white",
+        "bg": "#ffdd05", 
+    },
+    "4096": {
+        "font": "white",
+        "bg": "#323232", 
+    },
+}
